d2267d4b0f074d49f8df211fc4f0eef234187e1e
Jianan.Li <jianan.li@thoughtworks.com>
2020-10-15 23:29:16 +0800

    jianan - homework completed

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,9 @@
-const parseData
+const parseData = ({data, column}) => {
+
+    return data.map((info) => (
+        Object.assign({}, ...(info.map((value, index) => (
+            {[column[index].name]: value}
+        ))))
+    ));
+};
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,7 @@
-const inject
+const inject = (items, sections) => {
+    sections.forEach((value, index) => (
+        items.splice(value.index + index, 0, value.content)
+    ));
+    return items;
+}
 export { inject };
